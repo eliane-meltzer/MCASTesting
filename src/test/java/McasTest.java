e4d8b8c7ac4e886bc9122fb41b07f6a409775516
Eliane Zalewski <ezalewskiwilliams@gmail.com>
2020-11-27 14:55:37 -0500

    Mcas tests 1-3

--- src/test/java/McasTest.java
+++ src/test/java/McasTest.java
@@ -9,34 +9,42 @@ public class McasTest {
     @Test
     public void ActiveFFTT() {
         Mcas testMcas = new Mcas();
-        // assert statements
         assertEquals(1, 1);
-
+        testMcas.trim(false,false, 12.5);
+        try {
+            Thread.sleep(10000);
+        } catch (Exception e) {}
+        assertEquals(Mcas.Command.DOWN, testMcas.trim(false, false, 12.5));
+        assertEquals(Mcas.State.ACTIVE, testMcas.getState());
     }
 
     //  2
     @Test
     public void ActiveFFTF() {
         Mcas testMcas = new Mcas();
-        // assert statements
         assertEquals(1, 1);
-
+        testMcas.trim(false,false, 12.5);
+        assertEquals(Mcas.Command.NONE, testMcas.trim(false, false, 12.5));
+        assertEquals(Mcas.State.ACTIVE, testMcas.getState());
     }
 
     //  3
     @Test
     public void InactiveFTTT() {
         Mcas testMcas = new Mcas();
-        // assert statements
-        assertEquals(1, 1);
-
+        testMcas.trim(false,false, 12.5);
+        testMcas.trim(false, false, 12.5);
+        try {
+            Thread.sleep(10000);
+        } catch (Exception e) {}
+        assertEquals(Mcas.Command.NONE, testMcas.trim(false, true, 12.5));
+        assertEquals(Mcas.State.INACTIVE, testMcas.getState());
     }
 
 //     4
     @Test
     public void ActiveFFF_() {
         Mcas testMcas = new Mcas();
-        // assert statements
         testMcas.trim(false,false, 12.5);
         assertEquals(Mcas.Command.NONE, testMcas.trim(false, false, 11.5));
         assertEquals(Mcas.State.ARMED, testMcas.getState());
@@ -46,7 +54,6 @@ public class McasTest {
     @Test
     public void InactiveT_F_() {
         Mcas testMcas = new Mcas();
-        // assert statements
         assertEquals(Mcas.Command.NONE, testMcas.trim(true, false, 12.5));
         assertEquals(Mcas.State.INACTIVE, testMcas.getState());
     }
@@ -55,7 +62,6 @@ public class McasTest {
     @Test
     public void ActiveTFFF_() {
         Mcas testMcas = new Mcas();
-        // assert statements
         testMcas.trim(false,false, 12.5);
         assertEquals(Mcas.Command.NONE, testMcas.trim(true, false, 12.5));
         assertEquals(Mcas.State.INACTIVE, testMcas.getState());
@@ -65,7 +71,6 @@ public class McasTest {
     @Test
     public void ArmedFFT_() {
         Mcas testMcas = new Mcas();
-        // assert statements
         testMcas.trim(false,false, 11.5);
         assertEquals(Mcas.Command.DOWN, testMcas.trim(false, false, 12.5));
         assertEquals(Mcas.State.ACTIVE, testMcas.getState());
@@ -75,7 +80,6 @@ public class McasTest {
     @Test
     public void ArmedFFF_() {
         Mcas testMcas = new Mcas();
-        // assert statements
         testMcas.trim(false,false, 11.5);
         assertEquals(Mcas.Command.NONE, testMcas.trim(false, false, 11.5));
         assertEquals(Mcas.State.ARMED, testMcas.getState());
@@ -85,7 +89,6 @@ public class McasTest {
     @Test
     public void ArmedTFF_() {
         Mcas testMcas = new Mcas();
-        // assert statements
         testMcas.trim(false,false, 11.5);
         assertEquals(Mcas.Command.NONE, testMcas.trim(true, false, 11.5));
         assertEquals(Mcas.State.INACTIVE, testMcas.getState());
@@ -95,7 +98,6 @@ public class McasTest {
     @Test
     public void ArmedFTF_() {
         Mcas testMcas = new Mcas();
-        // assert statements
         testMcas.trim(false,false, 11.5);
         assertEquals(Mcas.Command.NONE, testMcas.trim(false, true, 11.5));
         assertEquals(Mcas.State.INACTIVE, testMcas.getState());
@@ -105,7 +107,6 @@ public class McasTest {
     @Test
     public void InactiveTF__() {
         Mcas testMcas = new Mcas();
-        // assert statements
         assertEquals(Mcas.Command.NONE, testMcas.trim(true, false, 11.5));
         assertEquals(Mcas.State.INACTIVE, testMcas.getState());
     }
@@ -114,7 +115,6 @@ public class McasTest {
     @Test
     public void InactiveFFF_() {
         Mcas testMcas = new Mcas();
-        // assert statements
         assertEquals(Mcas.Command.NONE, testMcas.trim(false, false, 11.5));
         assertEquals(Mcas.State.ARMED, testMcas.getState());
     }
